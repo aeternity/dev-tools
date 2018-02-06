3dd039afee5d9898436276fbf86e130479051795
John Newby <john@newby.org>
2018-02-06 14:37:34 +0100

    New balance interface

--- bash/aeternity-functions.sh
+++ bash/aeternity-functions.sh
@@ -28,10 +28,19 @@ function aeupdate_from_epoch_yaml {
 }
 
 alias aepub_key="curl -s http://127.0.0.1:$AE_LOCAL_INTERNAL_PORT/v2/account/pub-key|jq '.pub_key'|sed -e 's/\"//g'"
-alias aeupdate_pub_key="export AE_PUB_KEY=`aepub_key`"
+function aeupdate_pub_key {
+    export AE_PUB_KEY=\'`aepub_key`\'
+	  
+}
 aeupdate_pub_key
 alias aecd="cd $EPOCH_HOME"
-alias aebalance="curl -sG http://127.0.0.1:$AE_LOCAL_PORT/v2/account/balance --data-urlencode '\"pub_key=$AE_PUB_KEY\"'|jq .balance"
+alias aebalance="curl -sG http://127.0.0.1:$AE_LOCAL_INTERNAL_PORT/v2/account/balance/`aepub_key`|jq .balance"
+
+function aebalance {
+    PUB_KEY=`aepub_key`
+    curl -sG "http://127.0.0.1:$AE_LOCAL_INTERNAL_PORT/v2/account/balance/${PUB_KEY}" | jq .balance
+    
+}
 
 function aespend-tx {
     if [ "$#" -ne 3 ]; then
