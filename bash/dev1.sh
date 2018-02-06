5a779a1b9bd6d390ee945d51c59e4e6a3705dbdc
John Newby <john@newby.org>
2018-02-06 12:37:21 +0100

    Use relative paths

--- bash/dev1.sh
+++ bash/dev1.sh
@@ -1,5 +1,5 @@
 export AE_LOCAL_PORT=3013
 export AE_LOCAL_INTERNAL_PORT=3113
 export AE_WEBSOCKET=3114
-. ~/projects/aeternity/dev-tools/aeternity-functions.sh
+. ${BASH_SOURCE%/*}/aeternity-functions.sh
 
--- bash/dev2.sh
+++ bash/dev2.sh
@@ -1,4 +1,4 @@
 export AE_LOCAL_PORT=3023
 export AE_LOCAL_INTERNAL_PORT=3123
 export AE_WEBSOCKET=3124
-. ~/projects/aeternity/dev-tools/aeternity-functions.sh
+. ${BASH_SOURCE%/*}/aeternity-functions.sh
